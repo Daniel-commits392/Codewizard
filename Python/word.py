01f025f07fcb25471bdfcd1959ec29d3883e8e7b
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-08-11 00:47:33 +0300

    create function and add three variables

---
+++ Python/word.py
@@ -0,0 +1,5 @@
+def word_game(beginWord,endWord,wordList):
+    beginWord='hit'
+    endWord='cog'
+    wordList=['hot','dot','dog','lot','log','cog']
+    print(word_game(beginWord,endWord,wordList))    
