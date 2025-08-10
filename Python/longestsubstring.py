dff0a0d0c324f5e32c15b29ca2379d71654420a6
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-08-11 00:21:47 +0300

    finished on the if statements

--- Python/longestsubstring.py
+++ Python/longestsubstring.py
@@ -4,4 +4,12 @@ def longest_substring(s):
 
     for letter in s:
         if letter in current:
-            
+            current=''
+        current+=letter
+        if len(current)>longest:
+                longest = len(current)
+    return longest  
+
+print(longest_substring('bbbb'))    
+print(longest_substring('bjksdh')) 
+print(longest_substring('fhurimbb'))     
